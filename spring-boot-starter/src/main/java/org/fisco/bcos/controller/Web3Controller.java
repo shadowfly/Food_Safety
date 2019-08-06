d81dda2720fa0a4979c92c68af57cb14dc8aa6f1
林怡静 <linyijing@linyijingdeMBP.bbrouter>
2019-08-06 18:18:14 +0800

    add:contract.java

--- spring-boot-starter/src/main/java/org/fisco/bcos/controller/Web3Controller.java
+++ spring-boot-starter/src/main/java/org/fisco/bcos/controller/Web3Controller.java
@@ -1,11 +1,20 @@
 package org.fisco.bcos.controller;
 
+import org.fisco.bcos.contract.InfoTable;
+import org.fisco.bcos.web3j.crypto.Credentials;
+import org.fisco.bcos.web3j.crypto.Keys;
 import org.fisco.bcos.web3j.protocol.Web3j;
+import org.fisco.bcos.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.fisco.bcos.web3j.tuples.generated.Tuple2;
+import org.fisco.bcos.web3j.tx.gas.StaticGasProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.math.BigInteger;
+
+import static org.fisco.bcos.constants.GasConstants.GAS_LIMIT;
+import static org.fisco.bcos.constants.GasConstants.GAS_PRICE;
 
 @Component
 public class Web3Controller {
@@ -18,11 +27,24 @@ public class Web3Controller {
 
     @Bean
     public void test() throws Exception {
+        // 初始化Web3j对象
         BigInteger blockNumber = web3.getBlockNumber().send().getBlockNumber();
         System.out.println(blockNumber);
 
+        // 初始化Credentials对象
+        Credentials credentials = Credentials.create(Keys.createEcKeyPair());
+
         String resultHash = ipfs.upload();
         ipfs.download(resultHash);
+
+//        InfoTable asset = InfoTable.deploy(web3, credentials, new StaticGasProvider(GAS_PRICE, GAS_LIMIT)).send();
+        String contractAddress = "0xcdcce60801c0a2e6bb534322c32ae528b9dec8d2";
+        // 加载合约地址
+        InfoTable asset = InfoTable.load(contractAddress, web3, credentials, new StaticGasProvider(GAS_PRICE, GAS_LIMIT));
+        TransactionReceipt resultInsert = asset.retailInsert("0","store",resultHash).send();
+
+        TransactionReceipt resultSelect = asset.retailSelect("0").send();
+        System.out.println("retailSelect:"+resultSelect);
     }
 
 }
